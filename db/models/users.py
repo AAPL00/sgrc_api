b809b09bda8339679f20a8ad52f4595eefcc7a63
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 16:43:13 -0500

    Update users models for using EmailStr type

--- db/models/users.py
+++ db/models/users.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
     name: str
     password: str
-    email: str
+    email: EmailStr
     phone_number: str
     type: int #0-admin, 1-user
 
 class User_Response(BaseModel):
     name: str
-    email: str
+    email: EmailStr
     phone_number: str
     type: int #0-admin, 1-user
