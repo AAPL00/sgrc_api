549ecb2388f6d040c822f193d265c759f9aa6c19
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 12:38:48 -0500

    Update reservation model

--- db/models/reservation.py
+++ db/models/reservation.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class Reservation(BaseModel):
-    id_space: str
-    id_user: str
+    reserving_space: str
+    reserving_user: str
     start_date: datetime
     end_date: datetime 
     state: int #0-waiting, 1-confirmed, -1-canceled
