4f76976005ae7b1e04b79275b71dc72dad10b112
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 10:35:08 -0500

    Add user schema to repo

---
+++ db/schemas/user.py
@@ -0,0 +1,6 @@
+def user_schema(user) -> dict:
+    return {"name": user["name"],
+            "password": user["password"],
+            "email": user["email"],
+            "phone_number": user["phone_number"],
+            "type": user[type]}
