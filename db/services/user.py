55c61b258f0625a9e447e3c3e04c8d87f52a8cc1
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 12:39:25 -0500

    Update user service with schema deleted

--- db/services/user.py
+++ db/services/user.py
@@ -1,10 +1,4 @@
 from db.client import client_db
-from db.schemas.user import user_schema
 
 def search_user_by_name(name: str):
-    user = client_db.users.find_one({"name": name})
-    if not user:
-        user = None
-    else:
-        user = user_schema(user)
-    return user
+    return client_db.users.find_one({"name": name}, {"_id": 0})
