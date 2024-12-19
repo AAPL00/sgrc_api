e5e46480883b231bfae952b329516d9e73aa6e9b
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 15:48:33 -0500

    Update user service for excluding password from consult

--- db/services/user.py
+++ db/services/user.py
@@ -1,4 +1,4 @@
 from db.client import client_db
 
 def search_user_by_name(name: str):
-    return client_db.users.find_one({"name": name}, {"_id": 0})
+    return client_db.users.find_one({"name": name}, {"_id": 0, "pasword": 0})
