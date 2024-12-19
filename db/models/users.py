27afffa512869b6a65e6924b8422520cbc35d08f
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 15:47:53 -0500

    Add User_Response model to repo

--- db/models/users.py
+++ db/models/users.py
@@ -6,3 +6,9 @@ class User(BaseModel):
     email: str
     phone_number: str
     type: int #0-admin, 1-user
+
+class User_Response(BaseModel):
+    name: str
+    email: str
+    phone_number: str
+    type: int #0-admin, 1-user
