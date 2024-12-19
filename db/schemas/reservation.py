af9fb9f203f998450a526e31e942615ce79763ed
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 12:38:29 -0500

    Delete unnecesary schemas

--- db/schemas/reservation.py
+++
@@ -1,6 +0,0 @@
-def reservation_schema(reservation) -> dict:
-    return {"id_space": reservation["id_space"],
-            "id_user": reservation["id_user"],
-            "start_date": reservation["start_date"],
-            "end_date": reservation["end_date"],
-            "state": reservation["state"]}
--- db/schemas/user.py
+++
@@ -1,6 +0,0 @@
-def user_schema(user) -> dict:
-    return {"name": user["name"],
-            "password": user["password"],
-            "email": user["email"],
-            "phone_number": user["phone_number"],
-            "type": user["type"]}
