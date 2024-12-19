975c8d5006c13907cb5688d5f624849f72e6366e
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-19 11:13:19 -0500

    Add users router and post function

---
+++ routers/users.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, HTTPException, status
+from db.models.users import User
+from db.client import client_db
+from db.schemas.user import user_schema
+from db.services.user import search_user_by_name
+
+users_router = APIRouter(prefix="/users")
+
+@users_router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+async def save_user(user: User):
+    user_dict = dict(user)
+    if not search_user_by_name(user.name):
+        client_db.users.insert_one(user_dict)
+        return User(**search_user_by_name(user.name))
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists")
+    
+
