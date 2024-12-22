6c554e609bca191f104a6b1b7df6e7ed0ef14f4a
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2024-12-22 18:28:19 -0500

    Add users router to main api

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers.auth_system import auth_router
+from routers.users import users_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(users_router)
 
 @app.get("/")
 async def get():
