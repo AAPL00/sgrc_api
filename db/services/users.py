740ed6df36ab23c6d784deccc847099a24b6bee2
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2025-01-09 08:05:35 -0500

    add crypt context fto users service

--- db/services/users.py
+++ db/services/users.py
@@ -1,6 +1,8 @@
 from db.client import client_db
 from db.models.users import User_Response, User
-from db.services.auth_system import cript
+from passlib.context import CryptContext
+
+cript = CryptContext(schemes=["bcrypt"])
 
 def insert_user(user: User):
     user.password = cript.hash(user.password)
