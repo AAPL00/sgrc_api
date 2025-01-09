ea43b023c30fbfa63e1b5a3eccd924f51d0f1e69
aapl00 <antonioalejandroperezlorenzo9@gmail.com>
2025-01-09 08:05:55 -0500

    update auth system services

--- routers/auth_system.py
+++ routers/auth_system.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from db.services.auth_system import auth_user, create_acces_token
+from db.services.auth_system import login_verification, create_acces_token
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 auth_router = APIRouter()
@@ -7,7 +7,7 @@ oauth2 = OAuth2PasswordBearer(tokenUrl="login")
     
 @auth_router.post("/login", status_code=status.HTTP_202_ACCEPTED)
 async def login_user(form: OAuth2PasswordRequestForm = Depends()):
-    name = auth_user(form.username, form.password)
+    name = login_verification(form.username, form.password)
     if not name:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
     return create_acces_token(name)
